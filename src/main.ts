cb1616ff34348b6102c58852de2dba84eb1507d1
Alex Burner <aburner@apple.com>
2022-06-12 12:29:12 -0700

    Add tarot spread

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,38 @@ import './style.css'
 
 document.body.style.backgroundColor = bgColor
 
-const canvas = document.getElementById('canvas')
-if (!(canvas instanceof HTMLCanvasElement)) throw new Error('Bad #canvas')
-
 switch (document.location.pathname) {
-  case '/color-test':
+  case '/color-test': {
+    const canvas = document.createElement('canvas')
+    document.body.appendChild(canvas)
     colorTest(canvas)
     break
-  case '/fields':
+  }
+  case '/fields': {
+    const canvas = document.createElement('canvas')
+    document.body.appendChild(canvas)
     fields(canvas)
     break
+  }
   case '/tarot-front': {
+    const canvas = document.createElement('canvas')
+    document.body.appendChild(canvas)
     const hash = document.location.hash.substring(1) || undefined
     const hashN = hash ? Number(hash) : NaN
     const n = Number.isFinite(hashN) ? hashN : 12
     tarotFront(canvas, n)
+    break
+  }
+  case '/tarot-spread-front': {
+    document.body.style.backgroundColor = '#EEE'
+    document.body.style.padding = '50px'
+    for (let i = 0, l = 25; i < l; i++) {
+      const canvas = document.createElement('canvas')
+      canvas.style.margin = '50px'
+      canvas.style.display = 'inline-block'
+      document.body.appendChild(canvas)
+      tarotFront(canvas, i)
+    }
     break
   }
 }
