8c823eb2b2e12a3e48e9ded2ee2fac739dff04ad
Alex Burner <aburner@apple.com>
2022-07-25 20:35:58 -0700

    Terrain rings

--- src/drawTerrain.ts
+++ src/drawTerrain.ts
@@ -8,9 +8,11 @@ export const drawTerrain = ({
   seedRadiusScale,
   seedRadiusMin,
   noiseRadius,
-  // ringMax,
+  noiseCount,
+  ringMax,
   strokeWidth,
   strokeColor,
+  shellGap,
 }: {
   width: number
   height: number
@@ -18,15 +20,19 @@ export const drawTerrain = ({
   seedRadiusScale: number
   seedRadiusMin: number
   noiseRadius: number
+  noiseCount: number
   ringMax: number
   strokeWidth: number
   strokeColor: paper.Color
-}): paper.PathItem | undefined => {
-  // const noise2D = createNoise2D()
-  const paths: paper.Path[] = []
+  shellGap: number
+}): void => {
+  const noise2D = createNoise2D()
+  const layers: paper.Path[][] = []
 
   // Create random seeds and rings
   for (let i = 0; i < seedCount; i++) {
+    const stack = []
+
     // Create seed
     const x = Math.random() * width
     const y = Math.random() * height
@@ -37,62 +43,89 @@ export const drawTerrain = ({
       seedRadiusScale,
       seedRadiusMin,
       noiseRadius,
+      noiseCount,
     })
-    paths.push(seed)
+    stack.push(seed)
 
     // Create rings
-    // const ringCount = Math.round(((1 + noise2D(x, y)) / 2) * ringMax)
-    // let prevRing = seed
-    // for (let j = 0; j < ringCount; j++) {
-    //   prevRing = createRing(prevRing)
-    //   paths.push(prevRing)
-    // }
+    const ringCount = Math.round(((1 + noise2D(x, y)) / 2) * ringMax)
+    let prevRing = seed
+    for (let j = 0; j < ringCount; j++) {
+      prevRing = createRing(prevRing, seedCenter, shellGap)
+      stack.push(prevRing)
+    }
+
+    // Flip the stack and weave into layers
+    stack.reverse()
+    stack.forEach((path, index) => {
+      if (layers[index]) {
+        layers[index]?.push(path)
+      } else {
+        layers[index] = [path]
+      }
+    })
   }
 
-  // Unite all the paths
-  let union: paper.PathItem | undefined
-  paths.forEach((path) => {
+  // Unite all the paths in each layer
+  layers.forEach((paths) => {
+    let union: paper.PathItem | undefined
+
+    paths.forEach((path) => {
+      if (union) {
+        const old = union
+        union = union.unite(path)
+        old.remove()
+        path.remove()
+      } else {
+        union = path
+      }
+    })
+
+    // Style the union
     if (union) {
-      const old = union
-      union = union.unite(path)
-      old.remove()
-      path.remove()
-    } else {
-      union = path
+      union.strokeColor = strokeColor
+      union.strokeWidth = strokeWidth
     }
   })
-
-  // Style the union
-  if (union) {
-    union.strokeColor = strokeColor
-    union.strokeWidth = strokeWidth
-  }
-
-  return union
 }
 
-// const createRing = (seed: paper.Path): paper.Path => {
-//   seed.segments.forEach(segment => {
-//     segment.handleIn
-//   })
-// }
+const createRing = (
+  prevRing: paper.Path,
+  center: paper.Point,
+  shellGap: number,
+): paper.Path => {
+  const points = prevRing.segments.map((segment) => {
+    const vector = segment.point.subtract(center)
+    vector.length = shellGap
+    const point = segment.point.add(vector)
+    return point
+  })
+
+  const path = new paper.Path({
+    segments: points,
+    closed: true,
+  })
+
+  path.smooth()
+
+  return path
+}
 
 const createSeed = (args: {
   seedCenter: paper.Point
   seedRadiusScale: number
   seedRadiusMin: number
   noiseRadius: number
+  noiseCount: number
 }): paper.Path => {
   const noise2D = createNoise2D()
-  const pointCount = 100
-  const points = new Array(pointCount).fill(null).map((_, i) => {
+  const points = new Array(args.noiseCount).fill(null).map((_, i) => {
     // Walk 2D simplex noise in a circle
     // -> so that our randomness aligns beginning & end
-    const angle = (360 / pointCount) * i
+    const angle = (360 / args.noiseCount) * i
     const [x, y] = getCircleXY(args.noiseRadius, angle)
     const pointRadius =
       ((1 + noise2D(x, y)) / 2) * args.seedRadiusScale + args.seedRadiusMin
-    console.log('pointRadius', pointRadius)
 
     // Use that noise to distance our seed point from center
     const point = new paper.Point(
@@ -108,11 +141,9 @@ const createSeed = (args: {
     closed: true,
   })
 
-  console.log('created path')
-
   // Simplify the path points
   // http://paperjs.org/tutorials/paths/smoothing-simplifying-flattening/
-  path.simplify()
+  path.smooth()
 
   return path
 }
--- src/routes/terrain.ts
+++ src/routes/terrain.ts
@@ -16,24 +16,17 @@ export const terrain = (canvas: HTMLCanvasElement): void => {
   canvas.style.height = `${canvasH}px`
   paper.setup(canvas)
 
-  const path = drawTerrain({
+  drawTerrain({
     width: canvasW,
     height: canvasH,
-    seedCount: 10,
-    seedRadiusScale: 40,
-    seedRadiusMin: 30,
-    noiseRadius: 1,
-    ringMax: 10,
+    seedCount: 20,
+    seedRadiusScale: 50,
+    seedRadiusMin: 10,
+    noiseRadius: 1.2,
+    noiseCount: 30,
+    ringMax: 15,
     strokeWidth: 1,
     strokeColor,
+    shellGap: 40,
   })
-
-  console.log('path.position', path?.position)
-
-  if (path) {
-    console.log('path.position', path.position)
-    const rect = new paper.Path.Rectangle(path.bounds.clone())
-    rect.strokeWidth = 1
-    rect.strokeColor = strokeColor
-  }
 }
