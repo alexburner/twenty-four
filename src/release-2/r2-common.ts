400e5e6220557661b6e127f0559fa1d2054b9935
Alex Burner <alex.burner@gmail.com>
2022-11-08 21:59:16 -0800

    r2 adv restore full color spectrum

--- src/release-2/r2-common.ts
+++ src/release-2/r2-common.ts
@@ -21,6 +21,6 @@ export const getIntroHue = (n: number, total: number): number => {
 }
 
 export const getAdvancedHue = (n: number, total: number): number => {
-  const hue = ((360 * ((n - 1) / (total + 1))) % 360) - 0
+  const hue = ((360 * ((n - 1) / (total + 0))) % 360) - 0
   return hue
 }
