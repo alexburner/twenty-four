a90b13ee7351e8ec4a1e09cdb465cafdae2fab66
Alex Burner <alex.burner@gmail.com>
2022-12-18 10:42:40 -0800

    WIP: r3 intro: messing w colors/graphs

--- src/release-3/r3_intro-back.ts
+++ src/release-3/r3_intro-back.ts
@@ -34,21 +34,21 @@ export const r3IntroBack = (
     brightness: 1,
   } as paper.Color
 
-  let rybHue = ((360 * ((n - 1) / (total - 1))) % 360) - 15
-  const originalRybHue = rybHue
-  if (n === 1) rybHue -= 360 / (total - 1) / 4
-  if (n > 1) rybHue += 360 / (total - 1) / 1
-  if ([2, 3].includes(n)) rybHue -= 360 / (total - 1) / 4
-  if ([4].includes(n)) rybHue += 360 / (total - 1) / 4
-  if ([5, 6].includes(n)) rybHue += 360 / (total - 1) / 3
-  if (n > 5) rybHue -= 360 / (total - 1) / 2
-  if (n > 7) rybHue -= 360 / (total - 1) / 2
-  // if (n > 8) rybHue += 360 / (total - 1) / 6
-  if (n === 10) {
-    rybHue = originalRybHue
-    // Match (n === 1)
-    rybHue -= 360 / (total - 1) / 4
-  }
+  const rybHue = ((360 * ((n - 1) / (total + 1))) % 360) - 15
+  // const originalRybHue = rybHue
+  // if (n === 1) rybHue -= 360 / (total - 1) / 4
+  // if (n > 1) rybHue += 360 / (total - 1) / 1
+  // if ([2, 3].includes(n)) rybHue -= 360 / (total - 1) / 4
+  // if ([4].includes(n)) rybHue += 360 / (total - 1) / 4
+  // if ([5, 6].includes(n)) rybHue += 360 / (total - 1) / 3
+  // if (n > 5) rybHue -= 360 / (total - 1) / 2
+  // if (n > 7) rybHue -= 360 / (total - 1) / 2
+  // // if (n > 8) rybHue += 360 / (total - 1) / 6
+  // if (n === 10) {
+  //   rybHue = originalRybHue
+  //   // Match (n === 1)
+  //   rybHue -= 360 / (total - 1) / 4
+  // }
   swatchColor = getRYB(0, 0, rybHue, 0.9, 0.38) as unknown as paper.Color
   lightColor = getRYB(0, 0, rybHue, 1, 0.11) as unknown as paper.Color
 
--- src/release-3/r3_intro-face.ts
+++ src/release-3/r3_intro-face.ts
@@ -6,7 +6,6 @@ import {
   getPoints,
   getRadius,
 } from '../draw'
-import { getRYB } from '../util'
 import { getIntroHue } from './r3_common'
 
 const BLEED = 36
@@ -29,35 +28,35 @@ export const r3IntroFace = (
 
   const hue = getIntroHue(n, total)
 
-  let swatchColor = {
+  const swatchColor = {
     hue,
     saturation: 0.8,
     brightness: 1,
   } as paper.Color
 
-  let lightColor = {
+  const lightColor = {
     hue,
     saturation: 0.2,
     brightness: 1,
   } as paper.Color
 
-  let rybHue = ((360 * ((n - 1) / (total - 1))) % 360) - 15
-  const originalRybHue = rybHue
-  if (n === 1) rybHue -= 360 / (total - 1) / 4
-  if (n > 1) rybHue += 360 / (total - 1) / 1
-  if ([2, 3].includes(n)) rybHue -= 360 / (total - 1) / 4
-  if ([4].includes(n)) rybHue += 360 / (total - 1) / 4
-  if ([5, 6].includes(n)) rybHue += 360 / (total - 1) / 3
-  if (n > 5) rybHue -= 360 / (total - 1) / 2
-  if (n > 7) rybHue -= 360 / (total - 1) / 2
-  // if (n > 8) rybHue += 360 / (total - 1) / 6
-  if (n === 10) {
-    rybHue = originalRybHue
-    // Match (n === 1)
-    rybHue -= 360 / (total - 1) / 4
-  }
-  swatchColor = getRYB(0, 0, rybHue, 0.9, 0.42) as unknown as paper.Color
-  lightColor = getRYB(0, 0, rybHue, 1, 0.11) as unknown as paper.Color
+  // let rybHue = ((360 * ((n - 1) / (total - 1))) % 360) - 15
+  // const originalRybHue = rybHue
+  // if (n === 1) rybHue -= 360 / (total - 1) / 4
+  // if (n > 1) rybHue += 360 / (total - 1) / 1
+  // if ([2, 3].includes(n)) rybHue -= 360 / (total - 1) / 4
+  // if ([4].includes(n)) rybHue += 360 / (total - 1) / 4
+  // if ([5, 6].includes(n)) rybHue += 360 / (total - 1) / 3
+  // if (n > 5) rybHue -= 360 / (total - 1) / 2
+  // if (n > 7) rybHue -= 360 / (total - 1) / 2
+  // // if (n > 8) rybHue += 360 / (total - 1) / 6
+  // if (n === 10) {
+  //   rybHue = originalRybHue
+  //   // Match (n === 1)
+  //   rybHue -= 360 / (total - 1) / 4
+  // }
+  // swatchColor = getRYB(0, 0, rybHue, 0.9, 0.42) as unknown as paper.Color
+  // lightColor = getRYB(0, 0, rybHue, 1, 0.11) as unknown as paper.Color
 
   const x = canvasW / 2
   const y = x
@@ -71,13 +70,13 @@ export const r3IntroFace = (
   const swatch = container.clone()
   swatch.fillColor = swatchColor
 
-  const radius = getRadius(proximity, n)
+  const radius = getRadius(proximity, 10)
   const points = getPoints(center, radius, n)
 
   if (n > 1) {
     drawOutline({
       points,
-      fillColor: lightColor,
+      fillColor: swatchColor,
       strokeColor: lightColor,
       strokeWidth: (dotRadius * 2) / 4,
     })
