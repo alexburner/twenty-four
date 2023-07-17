0184588ba2a99d8f86ed0990589a9443cfc02c50
Alex Burner <alex.burner@gmail.com>
2023-07-16 23:46:53 -0700

    r5 d back: remove text, radius

--- src/release-5/r5_dimension-back.ts
+++ src/release-5/r5_dimension-back.ts
@@ -1,5 +1,5 @@
 import paper from 'paper'
-import { drawBleed, drawDots, drawLines, getPoints, getRadius } from '../draw'
+import { drawBleed, drawDots, drawLines, getPoints } from '../draw'
 import { oneDotRadius } from './r5_common'
 
 const BLEED = 36
@@ -9,8 +9,8 @@ const canvasH = 300 * 4.75 + BLEED * 2
 
 const strokeColor = '#333' as unknown as paper.Color
 const strokeWidth = 4
-// const radius = 80
-const proximity = 125
+const radiusx = 80
+// const proximity = 125
 
 export const r5DimensionBack = (
   canvas: HTMLCanvasElement,
@@ -37,21 +37,25 @@ export const r5DimensionBack = (
   swatch.fillColor = swatchColor as paper.Color
 
   const center = new paper.Point(canvasW / 2, canvasH / 2)
-  const radius = n > 1 ? getRadius(proximity, n) : 0
+  // const radius = n > 1 ? getRadius(proximity, n) : 0
+  const nBoost2 = n * 2
+  const radius = radiusx + nBoost2
   const points = getPoints(center, radius, n, true)
 
   const positionGroup = new paper.Group()
 
   points.forEach((_point, i) => {
     const shadowGroup = new paper.Group()
-    const circle = new paper.Path.Circle({
-      center,
-      radius,
-      strokeColor,
-      strokeWidth: strokeWidth * 0.5,
-      opacity: 0,
-    })
-    shadowGroup.addChild(circle)
+    if (n > 1) {
+      const circle = new paper.Path.Circle({
+        center,
+        radius,
+        strokeColor,
+        strokeWidth: strokeWidth * 0.5,
+        opacity: 0,
+      })
+      shadowGroup.addChild(circle)
+    }
     if (n === 1) {
       const dots = drawDots(points, strokeColor, oneDotRadius)
       shadowGroup.addChild(dots)
@@ -71,7 +75,6 @@ export const r5DimensionBack = (
     const pointGroup = new paper.Group()
     pointGroup.addChild(shadowGroup)
 
-    // TODO draw lines for all except i
     {
       const subPoints = [...points]
       subPoints.splice((i + 1) % points.length, 1)
@@ -92,8 +95,8 @@ export const r5DimensionBack = (
 
     if (n > 1) {
       // const spreadDistance = 1000 / (points.length - 1)
-      const spreadDistance = radius * 2.3
-      const nBoost = (total - n) * 50
+      const spreadDistance = radius * 2.67
+      const nBoost = (total - n) * (radius * 0.125)
       pointGroup.position.y += i * (spreadDistance + nBoost)
     }
 
@@ -101,34 +104,34 @@ export const r5DimensionBack = (
   })
 
   // dimensions
-  const leftTexts = ['', 'nothing', 'point', 'line', 'plane', 'volume']
-  const rightTexts = ['', 'point', 'line', 'plane', 'volume', 'bulk']
-  const leftText = leftTexts[n]?.split('').join(' ')
-  const rightText = rightTexts[n]?.split('').join(' ')
-  const textFontSize = 44
-  const xSpace = canvasW * 0.2 + BLEED
-  const ySpace = BLEED * 2 + textFontSize / 2 - 5
-  const leftPoint = new paper.Point([xSpace, canvasH - ySpace])
-  const rightPoint = new paper.Point([canvasW - xSpace, canvasH - ySpace])
-  new paper.PointText({
-    point: leftPoint,
-    content: leftText,
-    justification: 'center',
-    fillColor: strokeColor,
-    fontFamily: 'FuturaLight',
-    fontSize: textFontSize,
-  })
-  new paper.PointText({
-    point: rightPoint,
-    content: rightText,
-    justification: 'center',
-    fillColor: strokeColor,
-    fontFamily: 'FuturaLight',
-    fontSize: textFontSize,
-  })
+  // const leftTexts = ['', 'nothing', 'point', 'line', 'plane', 'volume']
+  // const rightTexts = ['', 'point', 'line', 'plane', 'volume', 'bulk']
+  // const leftText = leftTexts[n]?.split('').join('')
+  // const rightText = rightTexts[n]?.split('').join('')
+  // const textFontSize = 44
+  // const xSpace = canvasW * 0.2 + BLEED
+  // const ySpace = BLEED * 2 + textFontSize / 2 - 5
+  // const leftPoint = new paper.Point([xSpace, canvasH - ySpace])
+  // const rightPoint = new paper.Point([canvasW - xSpace, canvasH - ySpace])
+  // new paper.PointText({
+  //   point: leftPoint,
+  //   content: leftText,
+  //   justification: 'center',
+  //   fillColor: strokeColor,
+  //   fontFamily: 'FuturaLight',
+  //   fontSize: textFontSize,
+  // })
+  // new paper.PointText({
+  //   point: rightPoint,
+  //   content: rightText,
+  //   justification: 'center',
+  //   fillColor: strokeColor,
+  //   fontFamily: 'FuturaLight',
+  //   fontSize: textFontSize,
+  // })
 
   positionGroup.position.y = center.y
-  positionGroup.position.y -= textFontSize * 0.5
+  // positionGroup.position.y -= textFontSize * 0.5
 
   swatch.sendToBack()
 
