2d9241950fe8dd8a98855bf65ee07786a06ab46f
Alex Burner <alex.burner@gmail.com>
2024-06-20 21:03:32 -0700

    wip r8 hue spread star fill debug

--- src/release-8/r8_hue_spread.ts
+++ src/release-8/r8_hue_spread.ts
@@ -181,6 +181,28 @@ export const r8HueSpread = (
           fill.position.y += distance * (spread.children.length - _i - 1)
           positionGroup.addChild(fill)
           // fill.sendToBack()
+          const iTextPoint = childGroup.position.clone()
+          iTextPoint.x -= radius * 1.5
+          const iText = new paper.PointText({
+            point: iTextPoint,
+            content: _i,
+            justification: 'center',
+            fillColor: strokeColor,
+            fontFamily: 'FuturaLight',
+            fontSize,
+          })
+          positionGroup.addChild(iText)
+          const skipTextPoint = childGroup.position.clone()
+          skipTextPoint.x += radius * 1.5
+          const skipText = new paper.PointText({
+            point: skipTextPoint,
+            content: skip,
+            justification: 'center',
+            fillColor: strokeColor,
+            fontFamily: 'FuturaLight',
+            fontSize,
+          })
+          positionGroup.addChild(skipText)
         }
       } else if (factor) {
         outline = drawOutline({
