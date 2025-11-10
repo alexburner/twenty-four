310a9ac84f9e3f4cd83bae99a3b1b2b17ead4947
Alex Burner <alex.burner@gmail.com>
2025-11-09 17:20:36 -0800

    r9 wip: new iteration

--- src/release-9/r9-laser-whole.ts
+++ src/release-9/r9-laser-whole.ts
@@ -2,11 +2,13 @@ import paper from 'paper'
 import {
   drawBleed,
   drawGraphsAndShells,
+  drawOutline,
   drawZeroShells,
   getPoints,
   getRadius,
 } from '../draw'
 import { drawTerrain } from '../drawTerrain'
+import { getAdvancedHue } from './r9-shared'
 
 const BLEED = 36
 
@@ -15,6 +17,7 @@ const canvasH = 300 * 4.75 + BLEED * 2
 
 const graphColor = '#333'
 const graphThickness = 4
+const shellThickness = 2
 const shellGap = 36
 const proximity = 150
 // const dotRadius = shellGap * 0.45
@@ -23,23 +26,21 @@ const dashArray: [number, number] = [0, 2.6]
 export const r9LaserWhole = (
   canvas: HTMLCanvasElement,
   n: number,
-  _total: number,
+  total: number,
   waves: boolean,
 ): void => {
   canvas.style.width = `${canvasW}px`
   canvas.style.height = `${canvasH}px`
   paper.setup(canvas)
 
-  const shellColor = {
-    hue: 0,
-    saturation: 0,
-    brightness: 0,
-  }
+  const hue = getAdvancedHue(n, total)
+
+  const shellColor = new paper.Color('white')
 
   const swatchColor = {
-    hue: 0,
-    saturation: 0,
-    brightness: 1,
+    hue,
+    saturation: 0.42,
+    brightness: 0.99,
   }
 
   const x = canvasW / 2
@@ -57,33 +58,43 @@ export const r9LaserWhole = (
   const radius = getRadius(proximity, n)
   const points = getPoints(center, radius, n)
 
+  drawOutline({
+    points,
+    strokeColor: 'transparent',
+    strokeWidth: 0,
+    fillColor: 'white',
+  })
+
   if (waves && n === 0) {
     drawTerrain({
       width: canvasW,
       height: canvasH,
       seedCoords: [
         // bottom center
-        [0.5 * canvasW, canvasH + 0.125 * canvasH],
+        [0.5 * canvasW, canvasH * 1.1],
+        // [0.5 * canvasW, canvasH * 0.5],
       ],
       seedRadiusScale: shellGap * 2,
       seedRadiusMin: shellGap / 2,
       noiseRadius: 0.6,
       noiseCount: 60,
       ringCount: 100,
-      strokeWidth: 1,
-      strokeColor: shellColor as paper.Color,
+      strokeWidth: shellThickness,
+      strokeColor: shellColor,
       shellGap,
+      // omit: 1,
+      // opacityScale: 10,
     })
   } else if (n === 0) {
     drawZeroShells({
-      center: new paper.Point(center.x, center.y - 3),
+      center: new paper.Point(center.x, center.y),
       size: canvasH * 1.5,
       radius,
       shelln: 31,
       shellColor,
       shellGap,
       dashArray,
-      shellThickness: 2,
+      shellThickness,
     })
   }
 
